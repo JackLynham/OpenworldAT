fb619cf637761a8e523caae57c86ba065143269d
JackLynham <Jack2.Lynham@live.uwe.ac.uk>
2020-01-28 12:56:47 +0000

    Terrain Generated

--- OpenWorld AT/Assets/Scipts/MapGenerator.cs
+++ OpenWorld AT/Assets/Scipts/MapGenerator.cs
@@ -4,18 +4,91 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+
+    public enum Drawmode {NoiseMap , ColourMap};
+    public Drawmode drawmode;
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
 
+    public int octaves;
+
+    [Range(0,1)]
+    public float persistance;
+    public float Lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public TerainType[] Regions;
     public bool update;
+
+ 
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed,
+            noiseScale,octaves, persistance, Lacunarity, offset);
+
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentheight = noiseMap[x, y];
+                for (int i = 0; i < Regions.Length; i++)
+                {
+                    if(currentheight<= Regions[i].height)
+                    {
+                        colourMap[y * mapWidth + x] = Regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+
+        if(drawmode == Drawmode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.textureHeightMap(noiseMap));
+        }
+        if (drawmode == Drawmode.ColourMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureColorMap
+                (colourMap, mapWidth,mapHeight));
+        }
+
     }
-    
+
+
+    private void OnValidate()
+    {
+        if(mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if(mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+
+        if (Lacunarity < 1)
+        {
+            Lacunarity = 1;
+        }
+        if(octaves < 0)
+        {
+            Lacunarity = 0;
+        }
+
+    }
+    [System.Serializable]
+    public struct TerainType
+    {
+        public float height;
+        public string name;
+        public Color color;
+    }
 }
--- OpenWorld AT/Assets/Scipts/Noise.cs
+++ OpenWorld AT/Assets/Scipts/Noise.cs
@@ -2,34 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Noise 
+public static class Noise
 {
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float persitance, float lacunarity)
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, 
+        float scale, int octaves, float persitance, float lacunarity, Vector2 offset)
     {
         float[,] noiseMap = new float[mapWidth, mapHeight];
 
-        if(scale <= 0)
+        System.Random rand = new System.Random(seed);
+        Vector2[] OctaveOfsets = new Vector2[octaves];
+
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = rand.Next(-100000, 100000) +offset.x;
+            float offsetY = rand.Next(-100000, 100000)+ offset.y;
+            OctaveOfsets[i] = new Vector2(offsetX, offsetY);
+        }
+        if (scale <= 0)
         {
             scale = 0.0001f;
         }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float halfWidth = mapWidth / 2f;
+        float halfHeight = mapHeight / 2f;
+
         for (int y = 0; y < mapHeight; y++)
         {
             for (int x = 0; x < mapWidth; x++)
             {
                 float amplitude = 1;
                 float frrquency = 1;
-
+                float noiseHeight = 0;
 
                 for (int i = 0; i < octaves; i++)
                 {
-                   float testx = x/scale;
-                    float testy = y / scale;
+                    float testx = (x-halfWidth) / scale * frrquency 
+                        +OctaveOfsets[i].x;
 
-                    float perlin = Mathf.PerlinNoise(testx, testy);
-                    noiseMap[x, y] = perlin;
+                    float testy = (y-halfHeight) / scale * frrquency 
+                        + OctaveOfsets[i].y;
+
+                    float perlin = Mathf.PerlinNoise(testx, testy) * 2 - 1;
+                    noiseHeight += perlin * amplitude;
+
+                    amplitude *= persitance;
+                    frrquency *= lacunarity;
                 }
-              
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+                noiseMap[x, y] = noiseHeight;
             }
+
+        }
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                noiseMap[x, y] = Mathf.InverseLerp
+                    (minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            }
+
         }
         return noiseMap;
     }
